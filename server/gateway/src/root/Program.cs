58f4f5d2742bb6107e9ead88756d87b484a958a8
Milan <milan.sramek.ml@gmail.com>
2025-05-28 17:11:38 +0200

    Gateway logging

--- server/gateway/src/root/Program.cs
+++ server/gateway/src/root/Program.cs
@@ -1,16 +1,57 @@
-var builder = WebApplication.CreateBuilder(args);
+using HotChocolate.CostAnalysis;
 
-builder.Services.AddHttpClient("Fusion");
+using Serilog;
 
-builder.Services
-    .AddFusionGatewayServer()
-    .ConfigureFromFile("gateway.fgp")
-    // Note: AllowQueryPlan is enabled for demonstration purposes. Disable in production environments.
-    .ModifyFusionOptions(x => x.AllowQueryPlan = true);
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.WebHost.UseUrls("http://*:80");
-var app = builder.Build();
+try
+{
+    Log.Information("Starting application");
 
-app.MapGraphQL();
+    var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+    builder.Services
+        .AddHttpClient("Fusion")
+        .AddDefaultLogger();
+
+    builder.Services
+        .AddHttpClient("Try")
+        .AddDefaultLogger();
+
+    builder.Services
+        .AddFusionGatewayServer(disableDefaultSecurity: true)
+        .ConfigureFromFile("Gateway.fgp")
+        // Note: AllowQueryPlan is enabled for demonstration purposes. Disable in production environments.
+        .ModifyFusionOptions(_ =>
+        {
+            _.AllowQueryPlan = true;
+            _.IncludeDebugInfo = true;
+        });
+
+    builder.Services.AddSingleton(new RequestCostOptions(100, 100, true, 10));
+
+    builder.WebHost.UseUrls("http://*:80");
+    var app = builder.Build();
+
+    app.MapGraphQL();
+    app.MapGet("/try", async (HttpContext context) =>
+    {
+        var client = context.RequestServices.GetRequiredService<IHttpClientFactory>().CreateClient("Try");
+        var response = await client.GetAsync("http://localhost:8081/graphql?query={__typename}");
+        response.EnsureSuccessStatusCode();
+        await context.Response.WriteAsync(await response.Content.ReadAsStringAsync());
+    });
+
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
